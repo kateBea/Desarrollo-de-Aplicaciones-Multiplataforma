28b37e1263baf688770a43bb98fa8d4d06f56228
kateBea <55351037+kateBea@users.noreply.github.com>
2022-11-08 20:18:14 +0100

    Some changes from local folder ...

---
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #1/Ejercicio4.java
@@ -0,0 +1,26 @@
+/* Título: Ejercicio 4
+ * Algoritmo: Indicar el signo de un real
+ * Fecha: 08.11.2022
+ * Autor: Hugo
+ */
+
+import java.io.*;
+
+public class Ejercicio4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        float numero;
+
+        System.out.print("Introduzca el radio del circulo: ");
+        numero = Float.parseFloat(lector.readLine());
+
+        if (numero == 0.0f)
+            System.out.println("El número es nulo.");
+        else {
+            if (numero > 0.0f) 
+                System.out.println("El número es positivo.");
+            else 
+            System.out.println("El número es negativo.");
+        }
+    }
+}
---
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #1/Ejercicio5.java
@@ -0,0 +1,39 @@
+/* Título: Ejercicio 5
+ * Algoritmo: Resolver ecuación cuadrática
+ * Fecha: 08.11.2022
+ * Autor: Hugo
+ */
+
+import java.io.*;
+
+public class Ejercicio5 {
+    public static void main(String[] args) throws IOException {
+        float a;
+        float b;
+        float c;
+        float discriminant;
+
+        InputStreamReader inputReader = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(inputReader);
+        
+        // lectura de datos
+        System.out.print("Please enter the value of [ a ]: ");
+        a = Float.parseFloat(reader.readLine());
+        System.out.print("Please enter the value of [ b ]: ");
+        b = Float.parseFloat(reader.readLine());
+        System.out.print("Please enter the value of [ c ]: ");
+        c = Float.parseFloat(reader.readLine());
+
+        discriminant = (float)Math.pow(b, 2) - (4 * a * c);
+
+        if (discriminant < 0) {
+            System.out.print("The corresponding equation with coefficients ");
+            System.out.println("[ a ] = " + a + ", [ b ] = " + b + " and [ c ] = " + c);
+            System.out.println("does not have a solution in the set of real numbers (no real roots) ):");
+        }
+        else {
+            System.out.println("First solution [ + ]: " + ((-b + Math.sqrt(discriminant)) / 2.0f * a));
+            System.out.println("Second solution [ - ]: " + ((-b - Math.sqrt(discriminant)) / 2.0f * a));
+        }
+    }
+}
