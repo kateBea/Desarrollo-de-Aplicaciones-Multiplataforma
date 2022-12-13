e087b7c6013375541b6f71e406feef6ee8db6a79
kateBea <55351037+kateBea@users.noreply.github.com>
2022-12-13 06:39:30 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Java #4/Ejercicio1.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Java #4/Ejercicio1.java
@@ -1,49 +1,46 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class Ejercicio1 {
     private static final Random rand = new Random();
+    private static final int LIMITE_NUMEROS = 100;
+    private static final int COTA_INFERIOR = 0;
+    private static final int COTA_SUPERIOR = 100;
+    private static final char PAR = 'p';
+    private static final char IMPAR = 'i';
+
     public static void main(String[] args) {
-        final int LIMITE_NUMEROS = 100;
-        final int COTA_INFERIOR = 0;
-        final int COTA_SUPERIOR = 100;
-        final char PAR = 'p';
-        final char IMPAR = 'I';
-
-        int[] numeros = new int[LIMITE_NUMEROS];
         char[] paridad = new char[LIMITE_NUMEROS];
+        int[] numeros = new int[LIMITE_NUMEROS];
 
+        generarAleatorios(numeros);
+        Arrays.sort(numeros);
+        obtenerParidad(numeros, paridad);
+        imprimir(numeros, paridad);
+
+    }
+
+    public static void generarAleatorios(int[] numeros) {
         for (int index = 0; index < LIMITE_NUMEROS; ++index)
             numeros[index] = rand.nextInt(COTA_INFERIOR, COTA_SUPERIOR + 1);
+    }
 
-        ordenar(numeros);
-
-        for (int index = 0; index < LIMITE_NUMEROS; ++index) {
-            if (numeros[index] % 2 == 0) 
-                paridad[index] = PAR;
-            else
-                paridad[index] = IMPAR;
-        }
-
+    public static void imprimir(int[] numeros, char[] paridad) {
         for (int index = 1; index <= LIMITE_NUMEROS; ++index) {
-            System.out.print(numeros[index - 1] + paridad[index - 1] + "- ");
+            System.out.printf("%d%c- ", numeros[index - 1], paridad[index - 1]);
 
             if (index % 10 == 0)
                 System.out.println();
         }
-
     }
 
-    public static void ordenar(int[] numeros) {
-        int temporal;
+    public static boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
 
-        for (int i = 0; i < numeros.length - 1; i++) {
-            for (int j = i + 1; j < numeros.length; j++) {
-                if (numeros[i] > numeros[j]) {
-                    temporal = numeros[i];
-                    numeros[i] = numeros[j];
-                    numeros[j] = temporal;
-                }
-            }
-        }
+    public static void obtenerParidad(int[] numeros, char[] paridad) {
+        for (int index = 0; index < LIMITE_NUMEROS; ++index)
+            paridad[index] = (esPar(numeros[index]) ? PAR : IMPAR);
+
     }
 }
