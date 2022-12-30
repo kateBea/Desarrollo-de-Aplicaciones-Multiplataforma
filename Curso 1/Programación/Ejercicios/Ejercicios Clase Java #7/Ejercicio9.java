bbbcbb540a5730e21aff6f20c937226f62f3202b
kateBea <55351037+kateBea@users.noreply.github.com>
2022-12-30 04:34:17 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #7/Ejercicio9.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #7/Ejercicio9.java
@@ -5,15 +5,22 @@ public class Ejercicio9 {
     private static final BufferedReader lector = new BufferedReader(input);
     public static void main(String[] args) throws IOException {
         String[] telf = new String[2];
+        int id;
+        long digitos;
 
         do {
             System.out.print("Por favor introduce un teléfono fijo: ");
-            telf = lector.readLine().split("-");
+            telf = lector.readLine().trim().split("-");
 
             if (!controlPatron(telf))
                 System.out.print("Teléfono fijo no válido, inténtalo de nuevo.");
             else {
-                
+                id = Integer.parseInt(telf[0]);
+                digitos = Long.parseLong(telf[1]);
+
+                System.out.println("Código de comunidad: " + id);
+                System.out.println("Dígitos del número fijo: " + digitos);
+                break;
             }
         }
         while (true);
@@ -25,5 +32,14 @@ public class Ejercicio9 {
                 return false;
 
         return true;
+    }
+
+    public static boolean controlPatron(String[] fijo) {
+        boolean correcto = true;
+
+        correcto = fijo[0].equals("91");
+        correcto = correcto && fijo[1].length() == 7 && todoDigitos(fijo[1]);
+
+        return correcto;
     }
 }
