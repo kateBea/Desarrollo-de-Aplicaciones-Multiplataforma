99caa12167305b994b05cec183bf53977250b71b
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-13 07:30:09 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio10.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio10.java
@@ -1,29 +1,61 @@
+/* 
+ * Este programa calcula las ganancias anuales de una explotación ganadera 
+ * vendedora de leche. El mismo programa genera los datos sobre los cuales se
+ * trabaja posteriormente.
+ * 
+ * @author Hugo
+ * @version 1.0
+ * @date 13 de enero de 2013
+ * 
+ * 
+ */
+
 import java.util.Random;
 
 public class Ejercicio10 {
-    private static final int COTA_INF_LITROS = 1000;
-    private static final int COTA_SUP_LITROS = 1500;
+    private static final float COTA_INF_LITROS = 1000.0f;
+    private static final float COTA_SUP_LITROS = 1500.0f;
     private static final float COTA_INF_VENTA = 0.5f;
     private static final float COTA_SUP_VENTA = 1.5f;
     private static final Random rand = new Random();
 
     public static void main(String[] args) {
-        float[][] datos = new float[2][12];
+        float[][] datos;
 
-        generarDatos(datos);
+        datos = generarDatos();
         mostrarGanancias(datos);
     }
 
-    public static void generarDatos(float[][] datos) {
+    /*
+     * Genera datos aleatorios de ventas
+     * 
+     * @param None
+     * @return float[][] matriz que contiene los datos de venta generados
+     * @author Hugo
+     */
+    public static float[][] generarDatos() {
         final int MESES = 12;
+        // la primera fila representa la cantidad de litros vendidos cada mes
+        // la segunda fila representa el precio de venta de del mes
+        float[][] datos = new float[2][12];
 
         for (int i = 0; i < MESES; ++i) {
-            datos[0][i] = (float)rand.nextInt(COTA_INF_LITROS, COTA_SUP_LITROS);
+            datos[0][i] = rand.nextFloat(COTA_INF_LITROS, COTA_SUP_LITROS);
             datos[1][i] = rand.nextFloat(COTA_INF_VENTA, COTA_SUP_VENTA);
         }
+
+        return datos;
     }
 
-    public static void mostrarGanancias(float[][] datos ) {
+    /*
+     * Muestra por pantalla las ganacias obtenidas de una 
+     * serie de ventas realizadas en doce meses
+     * 
+     * @param datos representa los litros vendidos junto con el precio de venta
+     * @return void
+     * @authro Hugo
+     */
+    public static void mostrarGanancias(float[][] datos) {
         final int MESES = 12;
         float ganancias = 0;
 
--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio9.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio9.java
@@ -1,3 +1,14 @@
+/* 
+ * Este programa lee un entero de la entrada estándar de datos
+ * y dibuja un triángulo con el cateto opuesto a la hipotenusa de la lingitud 
+ * del valor introducido. El valor indica el número de caracteres a mostrar
+ * 
+ * @author Hugo
+ * @version 1.0
+ * @date 13 de enero de 2013
+ * 
+ * 
+ */
 import java.util.Scanner;
 
 public class Ejercicio9 {
@@ -18,15 +29,34 @@ public class Ejercicio9 {
         lector.close();
     }
 
+    /*
+     * Imprime un triángulo rectángulo con el cateto opuesto a la hipotenusa
+     * a la derecha de la pantalla.
+     * 
+     * @param n longitud del cateto
+     * @return void
+     * @author Hugo
+     */
     public static void imprimirTriangulo(int n) {
         for (int i = n - 1; i >= 0; --i) {
+            // bucle exterior controla el número de filas
+
             for (int j = n - i; j > 0; --j)
+                // controla el número de caracteres por fila
                 System.out.print("*");
             
             System.out.println();
         }
     }
 
+    /*
+     * Imprime un triángulo rectángulo con el cateto opuesto a la hipotenusa
+     * a la izquierda de la pantalla.
+     * 
+     * @param n longitud del cateto
+     * @return void
+     * @author Hugo
+     */
     public static void imprimirTrianguloInvertido(int n) {
         for (int i = n - 1; i >= 0; --i) {
             for (int k = 0; k < i; ++k)
