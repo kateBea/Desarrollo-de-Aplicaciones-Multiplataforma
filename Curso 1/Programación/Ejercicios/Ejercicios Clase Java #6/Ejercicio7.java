567a835c2c2a5a3e2fb7b8059b8fcbc541984c03
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-13 07:13:04 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio7.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio7.java
@@ -1,3 +1,14 @@
+/* 
+ * Este programa lee de la entrada estándar una cadena y un entero y muestra
+ * por pantalla la cadena tantas veces como indique el entero.
+ * 
+ * @author Hugo
+ * @version 1.0
+ * @date 13 de enero de 2013
+ * 
+ * 
+ */
+
 import java.io.*;
 
 public class Ejercicio7 {
@@ -10,12 +21,26 @@ public class Ejercicio7 {
         System.out.print("Introduce una cadena: ");
         cadena = lector.readLine();
 
-        System.out.print("Introduce el número de repeticiones: ");
-        numeroDeRepeticiones = Integer.parseInt(lector.readLine());
+        do {
+            System.out.print("Introduce el número de repeticiones: ");
+            numeroDeRepeticiones = Integer.parseInt(lector.readLine());
+
+            if (numeroDeRepeticiones < 0)
+                System.out.println("Error: El número de repeticiones debe ser un número natural o cero.\n");
+        }
+        while (numeroDeRepeticiones < 0);
 
         System.out.print(repetirCadena(cadena, numeroDeRepeticiones));
     }
 
+    /*
+     * Retorna el resultado de concatenar una cadena tantas veces como se indica
+     * 
+     * @param str La cadena a ser concatenada
+     * @param veces El número de veces que se debe repetir la cadena
+     * @return String el resultado de la concatenación
+     * @authro Hugo
+     */
     public static String repetirCadena(String str, int veces) {
         String resultado = new String();
         for (int i = 0; i < veces; ++i)
--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio8.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio8.java
@@ -1,3 +1,13 @@
+/* 
+ * 
+ * 
+ * @author Hugo
+ * @version 1.0
+ * @date 13 de enero de 2013
+ * 
+ * 
+ */
+
 public class Ejercicio8 {
     public static void main(String[] args) {
         for (int i = 0; i < 9; i += 3) {
