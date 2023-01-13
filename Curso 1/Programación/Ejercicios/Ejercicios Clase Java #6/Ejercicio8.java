953ca57bb9378e763db0c2b9156ae5f64349b0e1
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-13 07:15:25 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio8.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #6/Ejercicio8.java
@@ -1,5 +1,6 @@
 /* 
- * 
+ * Este programa muestra por pantalla las tablas de multiplicar
+ * del número 1 al número 9.
  * 
  * @author Hugo
  * @version 1.0
@@ -11,7 +12,9 @@
 public class Ejercicio8 {
     public static void main(String[] args) {
         for (int i = 0; i < 9; i += 3) {
+            // controla grupos de tres números
             for (int j = 0; j < 10; ++j) {
+                // por cada grupo de tres números imprime su tabla de multiplicar
                 System.out.printf("%d * %d = %d  |  %d * %d = %d  |  %d * %d = %d\n",
                     i + 1, j, ((i + 1) * j), i + 2, j, ((i + 2) * j), i + 3, j, ((i + 3) * j));
             }
