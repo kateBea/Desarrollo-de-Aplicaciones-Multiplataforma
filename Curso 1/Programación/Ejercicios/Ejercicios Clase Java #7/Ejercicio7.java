110f2cc616753fc24cdcbcdfb73776f044f28347
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-15 20:39:49 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase Java #7/Ejercicio7.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase Java #7/Ejercicio7.java
@@ -1,5 +1,17 @@
 /* 
+ * Este programa gestiona la información relativa a una persona, a destacar, el nombre
+ * la dirección, la edad, el peso y la altura. Para ello no os ofrece una serie de operaciones
+ * como el sorteo de personas por edad, peso, cálculo de la media de pesos, media de alturas, 
+ * entre otras operaciones que nos muestra el programa por pantalla. Los datos vienen
+ * representados mediante una matriz de String. Cada fila de la matriz represennta 
+ * los datos de una persona en formato de String y cada columna representa un campo de
+ * información como se indica a continuación:
  * 
+ * Nombre -> columna 0
+ * Dirección -> columna 1
+ * Edad -> columna 2
+ * Peso -> columna 3
+ * Altura -> columna 4
  * 
  * @author Hugo
  * @version 1.0
@@ -10,6 +22,12 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Ejercicio7 {
+    // Utilidades para la lectura de datos
+    private static final InputStreamReader input = new InputStreamReader(System.in);
+    private static final BufferedReader lector = new BufferedReader(input);
+
+    // Enumerador que indica un
+    // método de ordenamiento
     enum ORDEN {
         OrdenPeso,
         OrdenEdad,
@@ -19,10 +37,15 @@ public class Ejercicio7 {
         gestor();
     }
 
+    /*
+     * Lee de la enhtrada de datos la información relativa a una serie de personas
+     * y ofrece operaciones sobre este listado.
+     * 
+     * @param None
+     * @return void
+     * @author Hugo
+     */
     public static void gestor() throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader lector = new BufferedReader(input);
-
         int opcion;
         int limitePersonas;
         final int LIMITE_DATOS = 5;
@@ -61,28 +84,80 @@ public class Ejercicio7 {
         while (opcion != 9);
     }
 
+    /*
+     * Ordena una matriz de String acorde con el ORDEN indicado
+     * 
+     * @param personas matriz que contiene información a ser ordenada
+     * @return void
+     * @author Hugo
+     */
     public static void ordenar(String[][] personas, ORDEN orden) {
         ordenar(personas, orden, 0, personas.length - 1);
     }
 
+    /*
+     * Ordena una matriz de String acorde con el ORDEN indicado.
+     * Implementa ordenación por fusión. Los dos índices pasados como parámetro 
+     * representan un subconjunto de filas dentro del rango de filas de la matriz.
+     * Ambos índices están comprendidos en el rango [0, personas[0].length() - 1]
+     * 
+     * @param personas matriz a ser ordenada
+     * @param orden el tipo de ordenamiento
+     * @param left índice dentro del rango de filas de la matriz
+     * @param right índice dentro del rango de filas de la matriz
+     * @return void
+     * @author Hugo
+     */
     public static void ordenar(String[][] personas, ORDEN orden, int left, int right) {
+        // ordenamos si el intervalo entre left y right (ambos inclusos)
+        // contiene al menos un elemento
         if (left < right) {
+            // divide arreglo a la mitad del tamaño
+            // (right - left) + 1 es el número de elementos comprendido
+            // en el rango que se trata actualmente
             int mid = (left + right) / 2;
+
+            // ordenar mitad izquierda del rango actual del arreglo (la matriz es un arreglo de arreglos)
             ordenar(personas, orden, left, mid);
+
+            // ordenar mitad derecha del rango actual del arreglo
             ordenar(personas, orden, mid + 1, right);
-            ordenar(personas, orden, left, mid, right);
+
+            // combinar ambas partes
+            combinar(personas, orden, left, mid, right);
         }
     }
 
-    public static void ordenar(String[][] personas, ORDEN orden, int left, int mid, int right) {
+    /*
+     * Fusiona los dos subconjuntos de filas de forma ordenada dentro de la matriz.
+     * Los dos índices (left y right) pasados como parámetro representan un subconjunto de filas 
+     * dentro del rango de filas de la matriz. El índice mid hace de separador entre ambos
+     * subconjuntos de filas.
+     * 
+     * Los tres índices están comprendidos en el rango [0, personas[0].length() - 1]
+     * 
+     * @param personas matriz a ser ordenada
+     * @param orden el tipo de ordenamiento
+     * @param left índice dentro del rango de filas de la matriz
+     * @param right índice dentro del rango de filas de la matriz
+     * @return void
+     * @author Hugo
+     */
+    public static void combinar(String[][] personas, ORDEN orden, int left, int mid, int right) {
+        // tamaño del vector auxiliar
         int newSize = (right - left) + 1;
+        // índice que itera sobre el subconjnto de filas superior
         int idxSubvectorIzq = left;
+        // índice que itera sobre el conjunto de filas inferior
         int idxSubvectorDer = mid + 1; 
+        // índice que itera sobre el vector auxiliar
         int idxArrayAuxiliar = 0;
 
         String[][] arrayAuxiliar = new String[newSize][5];
 
         if (orden == ORDEN.OrdenEdad) {
+            // ordenación de la matriz por edad
+            // dado un personas[i] donde i es un índice, personas[i][2] representa la edad 
             while (idxSubvectorIzq <= mid && idxSubvectorDer <= right) {
                 if (ordenPorEdad(Integer.parseInt(personas[idxSubvectorIzq][2]), Integer.parseInt(personas[idxSubvectorDer][2])))
                     arrayAuxiliar[idxArrayAuxiliar++] = personas[idxSubvectorIzq++];
@@ -91,6 +166,8 @@ public class Ejercicio7 {
             }
         }
         else if (orden == ORDEN.OrdenPeso) {
+            // ordenación de la matriz por peso
+            // dado un personas[i] donde i es un índice, personas[i][3] representa el peso
             while (idxSubvectorIzq <= mid && idxSubvectorDer <= right) {
                 if (ordenPorPeso(Float.parseFloat(personas[idxSubvectorIzq][3]), Float.parseFloat(personas[idxSubvectorDer][3])))
                     arrayAuxiliar[idxArrayAuxiliar++] = personas[idxSubvectorIzq++];
@@ -99,19 +176,45 @@ public class Ejercicio7 {
             }
         }
 
+        // copia elementos restantes de las filas superiores
+        // al arreglo auxiliar
         if (idxSubvectorIzq <= mid)
             System.arraycopy(personas, idxSubvectorIzq, arrayAuxiliar, idxArrayAuxiliar, (mid - idxSubvectorIzq) + 1);
-            
+        
+        // copia elementos restantes de las filas inferiores
+        // al arreglo auxiliar
         if (idxSubvectorDer <= right)
             System.arraycopy(personas, idxSubvectorDer, arrayAuxiliar, idxArrayAuxiliar, (right - idxSubvectorDer) + 1);
 
+        // copia los elementos ya ordenados del arreglo auxiliar a la matriz
         System.arraycopy(arrayAuxiliar, 0, personas, 0 + left, newSize);
     }
 
+    /*
+     * Esta función devuelve cierto si el primero valor es más grande 
+     * que el segundo. Se asume que ambos parámetros son números reales.
+     * 
+     * @param a valor a ser evaluado
+     * @param b valor a ser evaluado
+     * 
+     * @return boolean cierto si a es más grande que b. Falso en caso contrario
+     * @author Hugo
+     */
     public static boolean ordenPorPeso(float a, float b) {
         return a <= b;
     }
 
+    /*
+     * Esta función devuelve cierto si el primero valor es más grande 
+     * que el segundo. Funciona similar a orden por peso pero en este caso
+     * recibe números enteros.
+     * 
+     * @param a valor a ser evaluado
+     * @param b valor a ser evaluado
+     * 
+     * @return boolean cierto si a es más grande que b. Falso en caso contrario
+     * @author Hugo
+     */
     public static boolean ordenPorEdad(int a, int b) {
         return a <= b;
     }
@@ -125,8 +228,6 @@ public class Ejercicio7 {
     }
 
     public static void leerPersona(String[] persona) throws IOException {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader lector = new BufferedReader(input);
         System.out.print("Introduzca el nombre: ");
         persona[0] = lector.readLine();
         System.out.print("Introduzca la dirección: ");
@@ -234,6 +335,14 @@ public class Ejercicio7 {
         System.out.println("La persona con mayor peso es: " + personas[indiceMax][0]);
     }
 
+    /*
+     * Dada una matriz con la información de personas, muestra por
+     * pantalla la media aritmética del peso de todas las del listado.
+     * 
+     * @param personas matriz que contiene información sobre personas
+     * @return void
+     * @author Hugo
+     */
     public static void mostrarMediaPeso(String[][] personas) {
         float pesoMedio = 0;
         for (int i = 0; i < personas.length; ++i) 
@@ -242,6 +351,14 @@ public class Ejercicio7 {
         System.out.println("La media de peso es de: " + (pesoMedio / personas.length));
     }
 
+    /*
+     * Dada una matriz con la información de personas, muestra por
+     * pantalla la media aritmética de la edad de todas las del listado.
+     * 
+     * @param personas matriz que contiene información sobre personas
+     * @return void
+     * @author Hugo
+     */
     public static void mostrarMediaEdad(String[][] personas) {
         float edadMedia = 0;
         for (int i = 0; i < personas.length; ++i) 
@@ -251,30 +368,31 @@ public class Ejercicio7 {
     }
 
     public static void totalPersonasConMismaEdad(String[][] personas) {
-        // TODO. Descripción incompleta del problema para este caso. Varias interpretaciones
-
         // 1. Varios grupos de personas con misma edad (mostrar las que tienen mayor/menor edad? )
+        // 2. Mostrar todos indicando la cantidad para cada grupo.
         // ...
 
-        // guardará las edades de todas las personas
-        // en el pero de los casos todas las personas tienen edad diferente
+        // guardará las edades de las personas en el formato de
+        // entero para el proceso de conteo más delante
         int[] edades = new int[personas.length];
-        int indice = 0; // para iterar sobre el vector edades
-        int totalConEdad = 1; // cuenta el número de personas con una cierta edad
-        for (indice = 0; indice < edades.length; ++indice)
-            edades[indice] = -1;
 
-        indice = 0;
+        // para iterar sobre el vector edades
+        int indice = 0;
+
+        // cuenta el número de personas con una cierta edad
+        int totalConEdad = 1; 
+
         // obtener edades
         for (String[] persona : personas)
             edades[indice++] = Integer.parseInt(persona[2]);
 
+        // ordenar las edades para contarlas
+        // por grupos de menor edad a mayor edad
         Arrays.sort(edades);
-
-        // 22 22 22 55 55 55 88 88 91
         
         // totalConEdad vale 1 porque al entrar al bucle se asume
-        // que hay al menos una persona con la edad que se va a tratar
+        // que hay al menos una persona con la edad que se va a tratar y
+        // se repite esto cada vez que tenemos una edad nueva (cada grupo de edades)
         for (indice = 0; indice < edades.length - 1; ++indice) {
             if (edades[indice] == edades[indice + 1])
                 totalConEdad++;
