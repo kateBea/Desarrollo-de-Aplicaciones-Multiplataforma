9c0512aaf2d11731030d4cf8d2ec2aa3a3da8345
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-24 11:07:35 +0100

    Some changes from local folder ...

---
+++ Curso 1/Bases de Datos/Ejercicios/Ejercicios Consultas #3/Sript DML.sql
@@ -0,0 +1,8 @@
+/****************************************************
+Este Script contiene la definición de una tabla sencilla
+perteneciente a una basa de datos MySQL.
+
+Autor: Hugo Pelayo
+Fecha: 24 de enero de 2023
+Fichero: Script DML.sql
+****************************************************/
