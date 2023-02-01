2e7b2e083e0d7230979a88f9c35a32ecd0127255
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-01 23:19:34 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase POO #2/Ejercicio10/PruebaTrSector.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase POO #2/Ejercicio10/PruebaTrSector.java
@@ -4,6 +4,27 @@ import Ejercicio4.Persona;
 
 public class PruebaTrSector {
     public static void main(String[] args) {
-        
+        Persona p1 = new Persona("Roberto", "Carlos", "Rodríguez", "123456789F", "Estudiante", 21);
+        Persona p2 = new Persona("Marta", "Bastos", null, "276491742K", "Electricista", 25);
+        Persona p3 = new Persona("Carlos", "Torres", "Pelayo", "472540185L", "Ingeniero", 27);
+        Persona p4 = new Persona("Rubén", "Santos", "Gisbert", "274562913G", "Estudiante", 17);
+        Persona p5 = new Persona("Silvia", "Giménez", "Da Silva", "397640123M", "Hacker", 22);
+
+        p1.mostrar();
+        p2.mostrar();
+        p3.mostrar();
+        p4.mostrar();
+        p5.mostrar();
+
+        System.out.println("********** ALGUNAS MODIFICACIONES **********");
+        p1.setDni("777222888Y");
+        p3.setNombre("Rubén");
+        p3.mostrar();
+        p1.mostrar();
+
+        p5.setEdad(19);
+        p5.mostrar();
+
+        System.out.println("********** Prueba TrabajaSector **********");
     }    
 }
