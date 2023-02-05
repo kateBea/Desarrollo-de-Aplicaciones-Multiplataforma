2b58f6858ba6af97331396c3a28bab3fae7c30ca
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-05 13:44:13 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Prácticas/Práctica #3/empleados/Persona.java
+++ Curso 1/Programación/Prácticas/Práctica #3/empleados/Persona.java
@@ -1,28 +1,29 @@
 package empleados;
 
 /**
- * <h2></h2>
+ * <h2>Representa una persona</h2>
+ * 
+ * Esta clase representa una persona recogiendo los datos más
+ * básicos de una. Se pueden consultar y modificar a través de los
+ * métodos proveídos
  * 
  * @author Hugo Pelayo
  * @version 1.0
  */
 
 public class Persona {
+    // DNI de esta persona
     private String m_Dni;
+    // Nombre completo de esta persona
     private String m_Nombre;
+    // Estado civil de esta persona
     private String m_EstadoCivil;
+    // Edad de esta persona
     private int m_Edad;
 
     // para comprobar que los estados civiles son válidos
     private static String[] ESTADOS_CIVILES = { "Soltero", "Casado" };
-
-    public Persona(String dni, String nombre, String estado, int edad) {
-        m_Dni = dni;
-        m_Nombre = nombre;
-        setEstado(estado);
-        setEdad(edad);
-    }
-
+    
     public boolean equals(Persona other) {
         return m_Dni.equalsIgnoreCase(other.m_Dni) &&
             m_Nombre.equalsIgnoreCase(other.m_Nombre) &&
@@ -30,6 +31,28 @@ public class Persona {
             m_Edad == other.m_Edad;
     }
 
+    /**
+     * Inicializa los datos de esta persona a partir de los
+     * que se pasan como parámetros
+     * 
+     * @param dni DNI que se asigna a esta persona
+     * @param nombre Nombre que se asigna a esta persona
+     * @param estado Estado que se asigna a esta persona
+     * @param edad Edad que se asigna a esta persona
+     */
+    public Persona(String dni, String nombre, String estado, int edad) {
+        m_Dni = dni;
+        m_Nombre = nombre;
+        setEstado(estado);
+        setEdad(edad);
+    }
+
+    /**
+     * Inicializa esta persona como copia de la que
+     * se pasa como parámetro
+     * 
+     * @param persona Persona de la que se copia
+     */
     public Persona(Persona persona) {
         m_Dni = persona.m_Dni;
         m_Nombre = persona.m_Nombre;
@@ -37,30 +60,53 @@ public class Persona {
         m_Edad = persona.m_Edad;
     }
 
+    /**
+     * Cumpleaños de esta persona
+     */
     public void cumpleanios() {
         System.out.println(m_Nombre + " tiene " + m_Edad + " años.");
     }
 
-    public String getDni() {
-        return m_Dni;
-    }
-
+    /**
+     * Devuelve el nombre de esta persona
+     * 
+     * @return Nombre de esta persona
+     */
     public String getNombre() {
         return m_Nombre;
     }
 
+    /**
+     * Devuelve el estado de esta persona
+     * 
+     * @return Estado de esta persona
+     */
     public String getEstado() {
         return m_EstadoCivil;
     }
 
+    /**
+     * Devuelve el NIF de esta persona
+     * 
+     * @return NIF de esta persona
+     */
     public String getNif() {
         return m_Dni;
     }
 
+    /**
+     * Devuelve la edad de esta persona
+     * 
+     * @return Edad de esta personas
+     */
     public int getEdad() {
         return m_Edad;
     }
 
+    /**
+     * 
+     * @param edad
+     */
     public void setEdad(int edad) {
         if (edad < 0) {
             System.out.println("Valor de edad no válido");
