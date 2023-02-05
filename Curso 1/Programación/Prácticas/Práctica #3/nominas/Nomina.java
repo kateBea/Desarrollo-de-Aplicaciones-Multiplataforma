7eefeed6db4b0ca1ccd2eccb31a9e2b55c58deee
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-05 02:20:01 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Prácticas/Práctica #3/nominas/Nomina.java
+++ Curso 1/Programación/Prácticas/Práctica #3/nominas/Nomina.java
@@ -6,29 +6,53 @@ import empleados.Departamento;
 import empleados.Empresa;
 
 /**
- * <h2></h2>
+ * <h2>Representa la nómina de un empleado</h2>
+ * 
+ * Esta clase representa la nómina de un Empleado. Permite gestionar
+ * el salario neto y el salario bruto de un empleado. 
  * 
  * @author Hugo Pelayo
  * @version 1.0
  */
 
 public class Nomina {
+    // Empresa a la que pertence el empleado con esta nómina
     private Empresa m_Empresa;
+    // Departamento al que pertenece el empleado con esta nómina
     private Departamento m_Departamento;
+    // Empleado al que pertenece esta nómina
     private Empleado m_Empleado;
+    // Fecha de emisión de esta nómina
     private LocalDate m_Fecha;
-    private double m_SalarioBruto;
+    // Salario bruto del empleado con esta nómina
+    private double m_SalarioBruto; 
+    // Retención sobre el salario bruto
     private double m_Retencion;
+    // Salario neto del empleado con esta nómina
     private double m_SalarioNeto;
 
+    /*
+     * Función auxiliar que calcula el salario neto de esta nómina
+     * a partir de la retención y el salario bruto
+     */
     private double calcularNeto() {
         return m_SalarioBruto - m_SalarioBruto * (m_Retencion / 100.0);
     }
 
+    /**
+     * Inicializa los atributos de esta nómina a partir de los que se
+     * pasan como parámetros
+     * 
+     * @param empresa Empresa que se asigna al empleado con esta nómina
+     * @param departamento Departamento que se asigna al empleado con esta nómina
+     * @param empleado Empleado al que pertenece esta nómina
+     * @param retencion Retención a aplicar sobre el salario bruto
+     */
     public Nomina(Empresa empresa, Departamento departamento, Empleado empleado, double retencion) {
         m_Empresa = empresa;
         m_Departamento = departamento;
         m_Empleado = empleado;
+        m_SalarioBruto = empleado.getSueldo();
         m_Fecha = LocalDate.now();
 
         if (!(m_Retencion >= 0.0 && m_Retencion <= 100.0))
@@ -37,10 +61,16 @@ public class Nomina {
         m_SalarioNeto = calcularNeto();
     }
 
+    /**
+     * 
+     * @param empleado
+     * @param retencion
+     */
     public Nomina(Empleado empleado, double retencion) {
         m_Empresa = empleado.getDepartamento().getEmpresa();
         m_Departamento = empleado.getDepartamento();
         m_Empleado = empleado;
+        m_SalarioBruto = empleado.getSueldo();
         m_Fecha = LocalDate.now();
 
         if (!(m_Retencion >= 0.0 && m_Retencion <= 100.0))
@@ -50,7 +80,6 @@ public class Nomina {
     }
 
     public double getSalarioBruto() {
-        // salario bruto
         return m_SalarioBruto;
     }
 
@@ -93,7 +122,13 @@ public class Nomina {
     }
 
     public void setSalarioBruto(double bruto) {
-        m_SalarioBruto = bruto;
+        if (!(bruto < 0.0)) {
+            m_SalarioBruto = bruto;
+            m_SalarioNeto = calcularNeto();
+        }
+        else 
+            System.out.println("Valor de salario bruto inválido");
+
     }
 
     public void setRetencion(double retencion) {
