fc85c895ccf2452af202ccab65814fceeac72bd9
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-05 14:36:51 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Prácticas/Práctica #3/MainNomina.java
+++ Curso 1/Programación/Prácticas/Práctica #3/MainNomina.java
@@ -5,17 +5,46 @@ import java.util.ArrayList;
 public class MainNomina {
     public static void main(String[] args) {
         ArrayList<Empresa> empresas;
-        ArrayList<Departamento> departamentos;
         ArrayList<Persona> personas;
         ArrayList<Empleado> empleados;
+        ArrayList<Departamento> departamentos;
 
         // inicializamos la lista de empresas
-
-        // incializamos la lista de departamentos
-        
+        empresas = getEmpresas();
         // inicializamos la lista de personas
-
+        personas = getPersonas();
         // inicializamos la lista de empleados con la lista de personas que tenemos
+        empleados = getEmpleados(personas);
+        // incializamos la lista de departamentos
+        departamentos = getDepartamentos(empleados, empresas);
+    }
 
+    public static ArrayList<Empresa> getEmpresas() {
+        ArrayList<Empresa> resultado = new ArrayList<>();
+
+
+        return resultado;
+    }
+
+    
+    public static ArrayList<Persona> getPersonas() {
+        ArrayList<Persona> resultado = new ArrayList<>();
+        
+        
+        return resultado;
+    }
+    
+    public static ArrayList<Empleado> getEmpleados(ArrayList<Persona> personas) {
+        ArrayList<Empleado> resultado = new ArrayList<>();
+        
+        
+        return resultado;
+    }
+
+    public static ArrayList<Departamento> getDepartamentos(ArrayList<Empleado> empleados, ArrayList<Empresa> empresas) {
+        ArrayList<Departamento> resultado = new ArrayList<>();
+    
+    
+        return resultado;
     }
 }
