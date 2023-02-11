c39498343057a429d52f00ab159c8f0ca8869ce0
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-11 23:08:19 +0100

    Some changes from local folder ...

--- Curso 1/Bases de Datos/Prácticas/Entrega #6/Ejercicios III/Script DML Instituto.sql
+++ Curso 1/Bases de Datos/Prácticas/Entrega #6/Ejercicios III/Script DML Instituto.sql
@@ -1,24 +1,38 @@
-# 1:
+# 4:
 SELECT Materias.Nombre, Cursos.Nombre, Profesor
 FROM Materias INNER JOIN Cursos ON CodigoCurso = Cursos.Codigo;
 
-# 2:
+# 5:
 SELECT COUNT(Materias.Nombre) AS 'Materias del Profe', Cursos.Nombre, Profesor
 FROM Materias INNER JOIN Cursos ON Codigocurso = Cursos.Codigo
 GROUP BY Profesor;
 
-# 3:
+# 6:
 SELECT Documento, Anio AS 'Año', Cuota, Materias.Nombre AS 'Nombre de Materia', Cursos.Nombre AS 'Nombre de Curso'
 FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
 ORDER BY Cursos.Nombre, Materias.Nombre;
 
-# 4:
-SELECT Profesor
-FROM Materias
-# NOTA: Hacemos group by por que se asume que el conjunto de alumnos
-# de un profesor son los que se registran con un documento, codigocurso y materias en concreto
-# , no se tiene en cuenta el año ya que un alumno podría estar repitiendo un curso
-WHERE EXISTS (SELECT Profesor
-				   FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
-				   GROUP BY Inscrito.documento,Inscrito.codigocurso,Inscrito.codigomateria)
+# 7:
+SELECT COUNT(DISTINCT Inscrito.documento) AS 'Alumnos por Profesor', Profesor
+FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
 GROUP BY Profesor;
+
+# 8:
+SELECT COUNT(DISTINCT Inscrito.documento) AS 'Alumnos por Materia', Materias.Nombre
+FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
+GROUP BY Materias.Nombre;
+
+# 9:
+SELECT Inscrito.documento AS 'Documento de Alumno', Cursos.Nombre, COUNT(DISTINCT Inscrito.CodigoMateria) AS 'Materias por Curso'
+FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
+GROUP BY Inscrito.documento, Cursos.Nombre;
+
+# 10:
+SELECT COUNT(DISTINCT Inscrito.documento) AS 'Total Alumnos distintos'
+FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo;
+
+# 11:
+SELECT COUNT(DISTINCT Documento), Materias.Nombre, Cursos.Nombre
+FROM Inscrito INNER JOIN Materias INNER JOIN Cursos ON Inscrito.CodigoCurso = Cursos.Codigo AND Inscrito.CodigoMateria = Materias.Codigo
+WHERE Cuota = 'n'
+GROUP BY Inscrito.CodigoCurso, Inscrito.CodigoMateria;
