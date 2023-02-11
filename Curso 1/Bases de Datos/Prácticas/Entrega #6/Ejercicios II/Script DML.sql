b193bd7ab7e6f55d7be0794d2f5ca01079989726
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-11 20:49:01 +0100

    Some changes from local folder ...

--- Curso 1/Bases de Datos/Prácticas/Entrega #6/Ejercicios II/Script DML.sql
+++ Curso 1/Bases de Datos/Prácticas/Entrega #6/Ejercicios II/Script DML.sql
@@ -187,3 +187,124 @@ WHERE Cif IN (SELECT  Cif
 			  WHERE Programa.Codigo = Registra.Codigo AND Medio = 'INTERNET');
               
 # 37:
+SELECT DISTINCT Cliente.Nombre, Programa.Nombre
+FROM Cliente, Registra, Programa
+WHERE Cliente.Dni = Registra.Dni AND Medio = 'INTERNET' AND Registra.Codigo = Programa.Codigo;
+
+# 38:
+SELECT DISTINCT Cliente.Nombre, Programa.Nombre, Medio, Comercio.Nombre
+FROM Cliente, Registra, Programa, Comercio
+WHERE Cliente.Dni = Registra.Dni AND Registra.Codigo = Programa.Codigo AND Registra.Cif = Comercio.Cif;
+
+# 39:
+SELECT Comercio.Ciudad
+FROM Comercio
+WHERE Cif IN (SELECT Cif
+			FROM Registra
+			WHERE Codigo IN (SELECT Codigo
+							FROM Programa
+							WHERE Codigo IN (SELECT Codigo
+											 FROM Fabricante, Desarrolla
+											 WHERE Fabricante.Id_Fab = Desarrolla.Id_Fab AND Fabricante.Nombre = 'Oracle')));
+                                             
+# 40:
+SELECT *
+FROM Cliente
+WHERE Dni IN (SELECT Dni 
+			  FROM Registra
+              WHERE Codigo IN (SELECT Codigo FROM Programa WHERE Nombre = 'Access' AND Version = 'XP'));
+              
+# 41:
+SELECT Nombre 
+FROM Fabricante
+WHERE Pais = (SELECT Pais
+			  FROM Fabricante
+			  WHERE Nombre = 'ORACLE');
+              
+# 42:
+SELECT Nombre
+FROM Cliente
+WHERE Edad = (SELECT Edad FROM Cliente WHERE Nombre = 'Pepe Pérez');
+
+# 43:
+SELECT *
+FROM Comercio
+WHERE Ciudad = (SELECT Ciudad FROM Comercio WHERE Nombre = 'FNAC');
+
+# 44:
+SELECT *
+FROM Cliente a, Registra b
+# Primera parte del condicional para excluir a Pepito perez de la consulta final
+# Como antes solo hay resultados para 'Pepito Pérez'
+WHERE a.Nombre <> 'Pepito Pérez' AND a.Dni = b.Dni AND b.Medio IN (SELECT DISTINCT Medio
+																   FROM Cliente c, Registra d
+																   WHERE c.Dni = d.Dni AND c.Nombre = 'Pepito Pérez' AND b.Medio = b.Medio);
+                                                                   
+# 45:
+SELECT DISTINCT COUNT(*)
+FROM Programa
+WHERE EXISTS (SELECT DISTINCT Nombre, Version FROM Programa);
+
+# 46:
+SELECT COUNT(*)
+FROM Cliente
+WHERE Edad > 40;
+
+# 47:
+SELECT SUM(Cantidad)
+FROM Distribuye
+WHERE Cif = 1;
+
+# 48:
+SELECT AVG(Cantidad) AS 'Media Programa Código 7'
+FROM Distribuye
+WHERE Codigo = 7;
+
+# 49:
+SELECT MIN(Cantidad) AS 'Mínima cantidad Programa Código 7'
+FROM Distribuye
+WHERE Codigo = 7;
+
+# 50:
+SELECT MAX(Cantidad) AS 'Máxima cantidad Programa Código 7'
+FROM Distribuye
+WHERE Codigo = 7;
+
+# 51:
+SELECT *
+FROM Comercio
+WHERE EXISTS (SELECT * FROM Distribuye WHERE Codigo = 7 AND Distribuye.Cif = Comercio.Cif);
+
+# 52:
+SELECT COUNT(*)
+FROM Registra
+WHERE Medio = 'INTERNET';
+
+# 53:
+SELECT SUM(Cantidad) AS 'Copias vendidas en Sevilla'
+FROM Distribuye
+WHERE EXISTS (SELECT * 
+			  FROM Comercio 
+              WHERE Ciudad = 'Sevilla' AND Distribuye.Cif = Comercio.Cif);
+              
+# 54:
+SELECT COUNT(*)
+FROM Desarrolla
+WHERE EXISTS (SELECT *
+			  FROM Fabricante 
+              WHERE Pais = 'Estados Unidos' AND Desarrolla.Id_Fab = Fabricante.Id_Fab);
+              
+# 55:
+SELECT UPPER(Nombre) AS 'Nombre Cliente', LENGTH(Nombre) AS 'Longitud Nombre'
+FROM Cliente;
+
+# 56:
+SELECT CONCAT(Nombre, ' ', IFNULL(Version, '')) AS 'Programa'
+FROM Programa;
+
+
+
+
+
+              
+
