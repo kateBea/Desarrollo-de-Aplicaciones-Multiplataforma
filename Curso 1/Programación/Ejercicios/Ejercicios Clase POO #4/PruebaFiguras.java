1b4f0728b1198504e4780a5ece0e24156bd4fb87
kateBea <55351037+kateBea@users.noreply.github.com>
2023-02-13 13:23:10 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Clase POO #4/PruebaFiguras.java
+++ Curso 1/Programación/Ejercicios/Ejercicios Clase POO #4/PruebaFiguras.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Random;
 
 public class PruebaFiguras {
     // Para lectura de datos
