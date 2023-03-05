ade6f1a1db60a06cb1b83fb6a3d4cf26ec0655bb
kateBea <55351037+kateBea@users.noreply.github.com>
2023-03-05 04:17:42 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicio Proveedor/Proveedor/Proveedor/src/proveedor/Principal.java
+++ Curso 1/Programación/Ejercicios/Ejercicio Proveedor/Proveedor/Proveedor/src/proveedor/Principal.java
@@ -7,31 +7,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Principal {
-    public static class Pair<T, U> {
-        T m_First;
-        U m_Second;
-
-        public Pair(T first, U second) {
-            m_First = first;
-            m_Second = second;
-        }
-
-        public T getFirst() {
-            return m_First;
-        }
-
-        public U getSecond() {
-            return m_Second;
-        }
-    }
     public static void main(String[] args) {
         // Inicialización datos
 
-        // Configurado por defecto al directory principal del proyecto
-        // printCurrentWorkingDirectory();
+        // Configurado por defecto al directorio principal del proyecto
+        printCurrentWorkingDirectory();
 
         // registrar productos del proveedor
-        ArrayList<Pair<String, Double>> datos = loadData("./src/Productos.txt");
+        ArrayList<Producto> datos = loadData("./src/Productos.txt");
 
         if (datos == null) {
             System.out.println("--------------------------------");
@@ -41,17 +24,44 @@ public class Principal {
         }
 
         // Solicitar productos
+        // loadStoreData se encarga de actualizar la cantidad
+        // de productos que tenemos solicitados
         ArrayList<Tienda> tiendas = loadStoreData(datos);
+        double importeTotal = 0.0;
+
+        for (Tienda tienda : tiendas) {
+            System.out.println("Tienda " + tienda.getNombre() + ':');
+
+            for (Producto producto : tienda.getProductos()) {
+                System.out.print("Producto solicitado: " + producto.getNombre());
+                System.out.print(" [" + producto.getCantidad() + " unidades]\n");
+            }
+
+            System.out.println("---------------------------------------");
+        }
+
+        // Todos los productos solicitados entre todas las tiendas
+        System.out.println("\nRESUMEN TOTAL PRODUCTOS ENCARGADOS");
+        for (Producto p : datos) {
+            System.out.print("Producto solicitado: " + p.getNombre());
+            System.out.print(" [" + p.getCantidad() + " unidades]\n");
+            System.out.printf("Importe total solicitud: %.3f\n", p.getCantidad() * p.getPrecio());
+            System.out.println("---------------------------------------");
+
+            importeTotal += p.getCantidad() * p.getPrecio();
+        }
+
+        System.out.println("\nIMPORTE TOTAL DE LOS PRODUCTOS SOLICITADOS: " + importeTotal);
 
     }
 
-    public static ArrayList<Tienda> loadStoreData(ArrayList<Pair<String, Double>> datos) {
+    public static ArrayList<Tienda> loadStoreData(ArrayList<Producto> datos) {
         final InputStreamReader input = new InputStreamReader(System.in);
         final BufferedReader lector = new BufferedReader(input);
-
         final int TOTAL_TIENDAS = 5;
+
         int idProducto = 0;
-        int cantidad = 0;
+        int cantidad;
         ArrayList<Tienda> retVal = new ArrayList<>(TOTAL_TIENDAS);
 
         retVal.add(new Tienda("Alampo"));
@@ -60,10 +70,11 @@ public class Principal {
         retVal.add(new Tienda("Tesla"));
         retVal.add(new Tienda("Virtues"));
 
+        // Para cada tienda vamos a registrar los productos que tiene encargados
         for (int i = 0; i < TOTAL_TIENDAS; ++i) {
-            // gestiona las solicitudes por tienda
             System.out.println("Tienda: " + retVal.get(i).getNombre());
             do{
+                // Leemos el índice de producto
                 System.out.print("¿Qué producto desea? ");
                 try {
                     idProducto = Integer.parseInt(lector.readLine());
@@ -72,9 +83,17 @@ public class Principal {
                     System.out.println("Formato de índice no válido. Inténtelo de nuevo.");
                     continue;
                 }
-                System.out.println(datos.get(idProducto - 1).getFirst() + '\n');
+
+                // Si el índice es -1 ya hemos acabado de trata esta tienda
+                if (idProducto == -1) {
+                    System.out.println();
+                    break;
+                }
+
+                System.out.println(datos.get(idProducto - 1).getNombre() + '\n');
                 System.out.print("¿Qué cantidad desea? ");
 
+                // Leemos la cantidad de producto
                 try {
                     cantidad = Integer.parseInt(lector.readLine());
                 }
@@ -82,24 +101,35 @@ public class Principal {
                     System.out.println("Formato de cantidad no válido. Inténtelo de nuevo.");
                     continue;
                 }
+
                 System.out.println();
-                retVal.get(i).addProducto(new Producto(datos.get(idProducto - 1).getFirst(),
-                                                       datos.get(idProducto - 1).getSecond(),
+
+                // añadimos el producto a la lista de productos solicitados por la tienda actual
+                retVal.get(i).addProducto(new Producto(datos.get(idProducto - 1).getNombre(),
+                                                       datos.get(idProducto - 1).getPrecio(),
                                                         cantidad));
+
+                // Actualizamos la cantidad solicitada del producto actual
+                // La cantidad del producto será la actual más la que haya solicitado la tienda o empresa
+                datos.get(idProducto - 1).setCantidad(datos.get(idProducto - 1).getCantidad() + cantidad);
 
             }
             while (idProducto != -1);
         }
 
-
         return retVal;
     }
 
-    public static ArrayList<Pair<String, Double>> loadData(String dataFilePath) {
+    /**
+     * Lee una lista de productos de un fichero
+     * @param dataFilePath directorio del fichero a procesar
+     * @return Lista de productos leídos donde en cada índice tenemos el nombre de producto y su precio
+     * */
+    public static ArrayList<Producto> loadData(String dataFilePath) {
         // El fichero con directorio debe contener una lista de productos
         // donde en cada línea hay el nombre de un producto seguido de dos puntos y el precio
         final BufferedReader buffer;
-        ArrayList<Pair<String, Double>> retVal = null;
+        ArrayList<Producto> retVal = null;
         String tempLine;
         try {
             // abrir fichero de datos
@@ -121,14 +151,19 @@ public class Principal {
         return retVal;
     }
 
-    public static Pair<String, Double> parseProduct(String product) {
-        Double value = null;
-        String name = null;
+    /**
+     * Interpreta una cadena separándola en nombre de producto y su precio
+     * @param product línea del producto
+     * @return Nombre de producto y su precio
+     * */
+    public static Producto parseProduct(String product) {
         String[] temp = product.split(":");
-
-        return new Pair<>(temp[0].trim(), Double.parseDouble(temp[1].trim()));
+        return new Producto(temp[0].trim(), Double.parseDouble(temp[1].trim()), 0);
     }
-    // helper func
+
+    /**
+     * Muestra por pantalla el directorio de trabajo actual
+     * */
     public static void printCurrentWorkingDirectory() {
         // For more: https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
         String workingDir = System.getProperty("user.dir");
--- Curso 1/Programación/Ejercicios/Ejercicio Proveedor/Proveedor/Proveedor/src/proveedor/Producto.java
+++ Curso 1/Programación/Ejercicios/Ejercicio Proveedor/Proveedor/Proveedor/src/proveedor/Producto.java
@@ -7,29 +7,88 @@ package proveedor;
  * @version 1.0
  * */
 public class Producto {
+    // Representa el nombre de este producto
     private String m_Nombre;
+    // Representa el precio de este producto
     private double m_Precio;
+    // Representa la cantidad de este producto
     private int m_Stock;
 
+    /**
+     * Inicializa el parámetro implícito a partir del producto
+     * que se pasa como parámetro
+     * @param other producto del que se hace copia
+     * */
     public Producto(Producto other) {
         this(other.m_Nombre, other.m_Precio, other.m_Stock);
     }
 
+    /**
+     * Inicializa este producto a partir de los datos que se
+     * pasa como parámetros
+     * @param nombre nombre de este producto
+     * @param precio precio de este producto
+     * @param cantidad cantidad de este producto
+     * */
     public Producto(String nombre, double precio, int cantidad) {
         m_Nombre = nombre;
-        m_Precio = precio;
-        m_Stock = cantidad;
+        m_Precio = Math.max(precio, 0.0); // Probar si el parámetro es negativo
+        m_Stock = Math.max(cantidad, 0); // Probar si el parámetro es negativo
     }
 
+    /**
+     * Devuelve el nombre de este producto
+     * @return nombre de este producto
+     * */
     public String getNombre() {
         return m_Nombre;
     }
 
+    /**
+     * Reemplaza el nombre de este producto por el que se pasa como parámetro
+     * @param nombre nuevo nombre de este producto
+     * */
+    public void setNombre(String nombre) {
+        if (nombre == null) {
+            System.out.println("Referencia de nombre nula...");
+            return;
+        }
+
+        m_Nombre = nombre;
+    }
+
+    /**
+     * Devuelve la cantidad de este producto
+     * @return cantidad de este producto
+     * */
     public int getCantidad() {
         return m_Stock;
     }
 
+    /**
+     * Reemplaza la cantidad de este producto por la que se pasa como parámetro
+     * @param cantidad nueva cantidad de este producto
+     * */
     public void setCantidad(int cantidad) {
         m_Stock = cantidad;
+    }
+
+    /**
+     * Devuelve el precio de este producto
+     * @return precio de este producto
+     * */
+    public double getPrecio() { return m_Precio; }
+
+    /**
+     * Reemplaza el precio de este producto por el que se pasa como parámetro
+     * @param precio nuevo precio de este producto
+     * */
+    public void setPrecio(double precio) {
+        if (precio < 0.0) {
+            System.out.println("Valor de precio inválido...");
+            return;
+        }
+
+        m_Precio = precio;
     }
 }
