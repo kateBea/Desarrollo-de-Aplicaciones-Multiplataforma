6969cad8f80db414d95cfe513572aebcbd42a89d
kateBea <55351037+kateBea@users.noreply.github.com>
2023-03-09 12:18:43 +0100

    Some changes from local folder ...

--- Curso 1/Programación/Ejercicios/Ejercicios Python POO #2/publisher.py
+++
@@ -1,8 +0,0 @@
-class Publisher:
-    def __int__(self, pub_id, address, name):
-        self.pub_id = pub_id
-        self.address = address
-        self.name = name
-
-    def __str__(self):
-        return f'Id: {self.pub_id} Address: {self.address} Name: {self.name}'
