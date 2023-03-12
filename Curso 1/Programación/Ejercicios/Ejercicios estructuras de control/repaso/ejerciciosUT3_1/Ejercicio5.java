b703dca957f86c04f67e0479593a058cc1b00612
kateBea <55351037+kateBea@users.noreply.github.com>
2023-03-12 15:53:09 +0100

    Some changes from local folder ...

---
+++ Curso 1/Programación/Ejercicios/Ejercicios estructuras de control/repaso/ejerciciosUT3_1/Ejercicio5.java
@@ -0,0 +1,20 @@
+package ejerciciosUT3_1;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int n, muestra = 1;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Muestra: " + (muestra));
+
+            System.out.print("Entra número: ");
+            n = sc.nextInt();
+
+        }
+        while(n != muestra++);
+
+    }
+}
