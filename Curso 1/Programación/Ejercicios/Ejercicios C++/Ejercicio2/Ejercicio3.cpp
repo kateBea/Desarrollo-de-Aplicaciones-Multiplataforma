e5fd8e9a57096ca5d5462fd6c42da5823c21dc6d
kateBea <55351037+kateBea@users.noreply.github.com>
2023-05-08 01:56:37 +0200

    some changes

--- Curso 1/Programación/Ejercicios/Ejercicios C++/Ejercicio2/Ejercicio3.cpp
+++ Curso 1/Programación/Ejercicios/Ejercicios C++/Ejercicio2/Ejercicio3.cpp
@@ -6,78 +6,128 @@
 */
 #include <iostream>
 #include <algorithm>
-#include <new>
+#include <string>
+#include <list>
+#include <sstream>
 
-void rellerArray(char* numero, int size) {
+auto leerNumero() -> std::list<char> {
+    std::list<char> result;
+    std::string line{};
     std::cout << "Introduce número: ";
-    for (int index{}; index < size; ++index)
-        std::cin >> numero[index];
+    std::getline(std::cin, line);
+
+    char temp{};
+    std::istringstream ss{ line };
+    while (ss >> temp)
+        result.push_back(temp);
+
+    return result;
 }
 
-int main(int, char**) {
-    int size1{};
-    int size2{};
+auto digito(char ch) -> int {
+    return ch - '0';
+}
 
-    std::cout << "Introduce tamaño del primer array: "; std::cin >> size1;
-    std::cout << "Introduce tamaño del segundo array: "; std::cin >> size2;
+auto caracter(int digit) -> char {
+    return '0' + digit;
+}
 
-    auto numero1{ new (std::nothrow) char[size1]{} };
-    auto numero2{ new (std::nothrow) char[size2]{} };
-    char* resultado{ nullptr };
+auto sumaDos(const std::list<char>& num1, const std::list<char>& num2) -> std::list<char> {
+    std::list<char> result;
+    auto itPrimero{ num1.rbegin() };
+    auto itSegundo{ num2.rbegin() };
 
-    if (numero1 && numero2) {
-        rellerArray(numero1, size1);
-        rellerArray(numero2, size2);
+    // para evitar excepciones al incrementar los iteradores
+    const auto endFirst{ num1.rend()-- };
+    const auto endSecond{ num2.rend()-- };
 
-        int index1{ size1 - 1 };
-        int index2{ size2 - 1 };
-        int indexRes{ std::max(size1, size2) - 1 };
-        bool carry{ false };
+    int sumaDigitos{};
+    bool hayCarry{ false };
 
-        resultado = new (std::nothrow) char[std::max(size1, size2)]{};
-
-        int temp{};
-        while (index1 >= 0 && index2 >= 0) {
-            temp =  (numero1[index1--] - '0') + (numero2[index2--] - '0') + (carry ? 1 : 0);
-            resultado[indexRes--] = '0' + (temp % 10);
-            carry = temp > 9;
-        }
-
-        std::cout << "carry?" << carry << std::endl;
-        // copiamos resto de dígitos del primer número
-        // ya no hace falta comprobar carry otra vez
-        while (index1 >= 0) {
-            if (carry) {
-                temp = (1 + numero1[index1--] - '0');
-                carry = false;
-            }
-            else 
-                temp = (numero1[index1--] - '0');
-
-            resultado[indexRes--] = '0' + (temp % 10);
-        }
-
-        // copiamos resto de dígitos del segundo número
-        // ya no hace falta comprobar carry otra vez
-        while (index2 >= 0) {
-            if (carry) {
-                temp = (1 + numero1[index1--] - '0');
-                carry = false;
-            }
-            else 
-                temp = (numero1[index1--] - '0');
-
-            resultado[indexRes--] = '0' + (temp % 10);
-        }
-
-        std::cout << "Resultado es: ";
-        for (int index{}; index < std::max(size1, size2); ++index)
-            std::cout << static_cast<int>(resultado[index] - '0') << ' ';
+    while (itPrimero != endFirst && itSegundo != endSecond) {
+        sumaDigitos = digito(*itPrimero) + digito(*itSegundo) + (hayCarry ? 1 : 0);
+        result.insert(result.begin(), caracter(sumaDigitos % 10));
+        hayCarry = sumaDigitos > 9;
+        ++itPrimero;
+        ++itSegundo;
     }
 
-    delete numero1;
-    delete numero2;
-    delete resultado;
+    // si tenemos carry al salir del bucle 
+    // pero nos quedan elementos por añadir
+    sumaDigitos = hayCarry ? 1 : 0;
+
+    if (itPrimero != num1.rend()) {
+        sumaDigitos += digito(*itPrimero);
+        hayCarry = false;
+
+        result.insert(result.begin(), caracter(sumaDigitos));
+        ++itPrimero;
+    }
+
+    if (itSegundo != num2.rend()) {
+        sumaDigitos += digito(*itSegundo);
+        hayCarry = false;
+
+        result.insert(result.begin(), caracter(sumaDigitos));
+        ++itSegundo;
+    }
+
+
+    // insertar elementos restantes si es necesario
+    if (itPrimero != num1.rend())
+        result.insert(result.begin(), itPrimero, num1.rend());
+
+    if (itSegundo != num2.rend())
+        result.insert(result.begin(), itSegundo, num2.rend());
+
+
+    // si hay carry pero no hay más dígitos de ambos números que insertar
+    // esto pasa cuando ambos números tienen la misma longitud
+    if (hayCarry)
+        result.insert(result.begin(), '1');
+
+    return result;
+}
+
+
+// formatear salida a gusto
+auto muestraSuma(std::list<char> num1, std::list<char> num2, std::list<char> res) -> void {
+    auto mostrar{
+        [](const std::list<char>& num1, const std::list<char>& num2) -> void {
+            for (const auto& it : num1)
+                std::cout << it << ' ';
+            std::cout << std::endl;
+            for (const auto& it : num2)
+                std::cout << it << ' ';
+        }
+    };
+
+    if (num1.size() >= num2.size()) {
+        if (res.size() == std::max(num1.size(), num2.size()))
+            res.insert(res.begin(), ' ');
+
+        for (int i{}; i < num1.size() - num2.size(); ++i)
+            num2.insert(num2.begin(), ' ');
+
+        num1.insert(num1.begin(), ' ');
+        num2.insert(num2.begin(), '+');
+
+
+        mostrar(num1, num2);
+        std::cout << '\n' << std::string(res.size() * 2, '-') << '\n';
+
+        for (const auto& it : res)
+            std::cout << it << ' ';
+    }
+    else
+        muestraSuma(num2, num1, res);
+}
+
+int main() {
+    auto numero1{ leerNumero() };
+    auto numero2{ leerNumero() };
+    auto result{ sumaDos(numero1, numero2) };
+    muestraSuma(numero1, numero2, result);
 
     return 0;
 }
---
+++ Curso 1/Programación/Ejercicios/Ejercicios C++/Ejercicio2/Ejercicio4.cpp
@@ -0,0 +1,6 @@
+/**
+ * Simula el algoritmo de suma con acarreo.
+ * Se aplica a cualquier base hasta base 16
+ * Hugo Pelayo
+ * 7 de mayo de 2023
+*/
