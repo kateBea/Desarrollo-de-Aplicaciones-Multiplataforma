3a0e27cf62079ad2bcb8b6751e360aebfcda1ada
ケイト <55351037+kateBea@users.noreply.github.com>
2024-01-28 22:35:31 +0100

    added proyecto android practica 2

---
+++ Curso 2/Programación multimedia y dispositivos móviles/Prácticas/Práctica 2/app/src/main/java/org/ventas/pizzamaanager/MainActivity.kt
@@ -0,0 +1,22 @@
+package org.ventas.pizzamaanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    // Lista de composables para preview
+}
