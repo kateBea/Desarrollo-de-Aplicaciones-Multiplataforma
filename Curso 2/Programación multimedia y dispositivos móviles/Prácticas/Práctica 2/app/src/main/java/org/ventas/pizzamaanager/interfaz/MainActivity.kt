dab3a8bfadf066b29f1c5ba8fba32247af854e46
ケイト <55351037+kateBea@users.noreply.github.com>
2024-02-05 18:53:50 +0100

    added ejercicios & apuntes 5 feb

--- Curso 2/Programación multimedia y dispositivos móviles/Prácticas/Práctica 2/app/src/main/java/org/ventas/pizzamaanager/interfaz/MainActivity.kt
+++ Curso 2/Programación multimedia y dispositivos móviles/Prácticas/Práctica 2/app/src/main/java/org/ventas/pizzamaanager/interfaz/MainActivity.kt
@@ -1,10 +1,11 @@
-package org.ventas.pizzamaanager
+package org.ventas.pizzamaanager.interfaz
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import org.ventas.pizzamaanager.interfaz.PantallaInicio
 
 
 class MainActivity : ComponentActivity() {
