16759147f6cc8eb478f7e3b60fbdd2dfd324626f
ケイト <55351037+kateBea@users.noreply.github.com>
2024-04-24 12:19:59 +0200

    dotnet example

---
+++ Curso 1/Programación/Ejercicios/Ejercicios Dot NET/Ejercicios básicos #1/Ejercicios básicos #1/Ejercicio2/Program.cs
@@ -0,0 +1,8 @@
+﻿string userInput = LeerCadena("Introduce tu nombre: ");
+Console.WriteLine($"Buenos días {userInput}");
+
+string? LeerCadena(string prompt) 
+{
+    Console.Out.WriteLine($"{prompt}");
+    return  Console.ReadLine();
+}
